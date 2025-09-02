4afe9798192e4ec4cfa4f522b68fb902790c0d8e
Francesco Spinella <francescospinella76@gmail.com>
2025-09-02 12:23:27 +0200

    Update GestioneBanca.java

--- GestioneBanca/src/GestioneBanca.java
+++ GestioneBanca/src/GestioneBanca.java
@@ -1,3 +1,4 @@
+import java.io.StringReader;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -6,16 +7,75 @@ public class GestioneBanca {
         Banca banca = new Banca();
 
         Scanner s = new Scanner(System.in);
-
         while (true){
-            System.out.println("\n-------- BANCA --------");
-            System.out.println("1. Crea conto");
-            System.out.println("2. Desposita");
-            System.out.println("3. Preleva");
-            System.out.println("4. Visualizza saldo");
-            System.out.println("5. Elimina conto");
-            System.out.println("6. Chiudi applicazione");
-            System.out.println("------------------------");
+            System.out.println("\n------------------ BANCA ------------------");
+            System.out.println("1. Accedi al tuo conto");
+            System.out.println("2. Apri conto");
+            System.out.println("3. Chiudi applicazione");
+            System.out.println("-------------------------------------------");
+
+            int comando = 0;
+            while (true){
+                try {
+                    System.out.print("Inserisci comando: ");
+                    comando = s.nextInt();
+                    s.nextLine();
+                    break;
+                }
+                catch (InputMismatchException e){
+                    System.out.println("\uD83D\uDD34 Errore: " + e.getMessage());
+                    System.out.println("⚠️ Scelta utente non valida");
+                    System.out.println("------------------------\n");
+                    s.nextLine();
+                }
+            }
+
+            switch (comando){
+                case 1: // Accedi
+                    System.out.println("\n--------------- ACCEDI AL TUO CONTO ----------------");
+                    int numeroConto = 0;
+                    while (true){
+                        try {
+                            System.out.print("Il tuo numero conto: ");
+                            numeroConto = s.nextInt();
+                            s.nextLine();
+                            banca.accedi(numeroConto);
+                            break;
+
+                        }
+                        catch (InputMismatchException e){
+                            System.out.println("\uD83D\uDD34 Errore: " + e.getMessage());
+                            System.out.println("⚠️ Errore: Input utente non valido");
+                            System.out.println("------------------------\n");
+                            s.nextLine();
+                        }
+                    }
+
+                    break;
+
+                case 2: // Apri conto
+                    System.out.println("\n--------------- APRI CONTO ----------------");
+                    System.out.print("Nome: ");
+                    String intestatario = s.nextLine();
+                    banca.creaConto(intestatario);
+                    break;
+
+                case 3: // Chiudi applicazione
+                    System.out.println("Chiusura dell'applicazione...");
+                    return;
+
+                default:
+                    System.out.println("⚠️ Comando utente non valido");
+            }
+        }
+
+/*        while (true){
+            System.out.println("\n------------------ MENU ------------------");
+            System.out.println("1. Crea conto\t\t\t5. Visualizza saldo");
+            System.out.println("2. Deposita\t\t\t\t6. Elimina conto");
+            System.out.println("3. Preleva\t\t\t\t7. Chiudi applicazione");
+            System.out.println("4. Effettua pagamento");
+            System.out.println("-------------------------------------------");
 
             int comando = 0;
             while (true){
@@ -35,14 +95,14 @@ public class GestioneBanca {
 
             switch (comando){
                 case 1: // Crea conto
-                    System.out.println("\n---- CREA CONTO ----");
+                    System.out.println("\n--------------- CREA CONTO ----------------");
                     System.out.print("Intestatario conto: ");
                     String intestatario = s.nextLine();
                     banca.creaConto(intestatario);
                     break;
 
                 case 2: // Deposita
-                    System.out.println("\n---- DEPOSITA ----");
+                    System.out.println("\n---------------- DEPOSITA -----------------");
                     int numeroConto = 0;
                     double importo = 0.0;
                     while (true){
@@ -66,7 +126,7 @@ public class GestioneBanca {
                     break;
 
                 case 3: // Preleva
-                    System.out.println("\n---- DEPOSITA ----");
+                    System.out.println("\n----------------- PRELEVA -----------------");
                     numeroConto = 0;
                     while (true){
                         try {
@@ -88,8 +148,36 @@ public class GestioneBanca {
                     banca.effettuaPrelievo(numeroConto, importo);
                     break;
 
-                case 4: // Visualizza saldo
-                    System.out.println("\n---- VISUALIZZA SALDO ----");
+                case 4: // Effettua pagamento
+                    System.out.println("\n---------- EFFETTUA UN PAGAMENTO ----------");
+                    int numeroContoPagante = 0;
+                    int numeroContoRicevente = 0;
+                    importo = 0.0;
+                    while (true){
+                        try {
+                            System.out.println("Numero conto mittente: ");
+                            numeroContoPagante = s.nextInt();
+                            s.nextLine();
+                            System.out.println("Numero conto destinatario: ");
+                            numeroContoRicevente = s.nextInt();
+                            s.nextLine();
+                            System.out.println("Importo da pagare: €");
+                            importo = s.nextDouble();
+                            s.nextLine();
+                            break;
+                        }
+                        catch (InputMismatchException e){
+                            System.out.println("\uD83D\uDD34 Errore: " + e.getMessage());
+                            System.out.println("⚠️ Input utente non valido");
+                            System.out.println("------------------------\n");
+                            s.nextLine();
+                        }
+                    }
+                    banca.effettuaPagamento(numeroContoPagante, numeroContoRicevente, importo);
+                    break;
+
+                case 5: // Visualizza saldo
+                    System.out.println("\n------------ VISUALIZZA SALDO -------------");
                     numeroConto = 0;
                     while (true){
                         try {
@@ -108,8 +196,8 @@ public class GestioneBanca {
                     banca.visualizzaSaldo(numeroConto);
                     break;
 
-                case 5: // Elimina conto
-                    System.out.println("\n---- ELIMINA CONTO ----");
+                case 6: // Elimina conto
+                    System.out.println("\n-------------- ELIMINA CONTO --------------");
                     numeroConto = 0;
                     while (true){
                         try {
@@ -128,13 +216,13 @@ public class GestioneBanca {
                     banca.eliminaConto(numeroConto);
                     break;
 
-                case 6: // Chiudi applicazione
+                case 7: // Chiudi applicazione
                     System.out.println("Chiusura dell'applicazione...");
                     return;
 
                 default:
-                    System.out.println("Scelta utente non valida");
+                    System.out.println("⚠️ Comando utente non valido");
             }
-        }
+        } */
     }
 }
