fbcca6ae4038dcbbcdc3ff8c813c6501c1118fe6
Francesco Spinella <francescospinella76@gmail.com>
2025-09-04 18:53:26 +0200

    Update Transazioni.java

--- GestioneBanca/src/Transazioni.java
+++ GestioneBanca/src/Transazioni.java
@@ -1,4 +1,6 @@
 public interface Transazioni{
+    // Metodo deposita
     void deposita(double importo, boolean mostraMessaggio);
+    //Metodo preleva
     void preleva( double importo, boolean mostraMessaggio);
 }
